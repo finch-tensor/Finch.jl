0b5d13488b3fab6db943ddcdfe295f67b09ba6ee
Adrian Gushin <adrian.gushin@emory.edu>
2026-05-04 12:41:34 -0400

    undo

--- src/tensors/levels/sparse_bytemap_levels.jl
+++ src/tensors/levels/sparse_bytemap_levels.jl
@@ -680,10 +680,7 @@ Base.@propagate_inbounds function merge_bytemap(
             end
 
             nz_id = j + idx_id
-            if nz_id > length(srt[proc_id][nz_id])
-                break
-            end
-            
+
             idx = last(srt[proc_id][nz_id])
             pos = first(srt[proc_id][nz_id])
             lvl_srt[offset] = (pos, idx)
